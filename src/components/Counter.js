f9297890267e99abd56afa7bbb11c91e48432b01
Sahil <leviack629@gmail.com>
2022-09-24 14:06:01 +0530

    Modified Counter.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -21,11 +21,21 @@ class Counter extends Component {
     console.log(this.state.count) //synchronous function
   }
 
+  incrementByFive() {
+    this.setState({
+      count: this.state.count + 5
+    },
+    () => {
+      console.log("Callback Value by 5:", this.state.count)
+    })
+  }
+
   render() {
     return (
       <div>
         <h1>Count - {this.state.count}</h1>
         <button onClick={ () => this.increment()}>INCREMENT</button>
+        <button onClick={() => this.incrementByFive()}>INCREMENT BY 5</button>
       </div>
     )
   }
